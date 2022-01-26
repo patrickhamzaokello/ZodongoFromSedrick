01e31d4ea0cac3c2c34234f1c95bd90a72e8f6d7
sedrickotolo <sedricksedu2@gmail.com>
2022-01-26 21:00:53 +0300

    place order

--- app/src/main/java/com/pkasemer/zodongofoods/PlaceOrder.java
+++ app/src/main/java/com/pkasemer/zodongofoods/PlaceOrder.java
@@ -1,5 +1,6 @@
 package com.pkasemer.zodongofoods;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class PlaceOrder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_order);
+
+        ActionBar actionBar = getSupportActionBar(); // or getActionBar();
+        getSupportActionBar().setTitle("Zodongo Foods"); // set the top title
+        String title = actionBar.getTitle().toString(); // get the title
+        actionBar.hide();
     }
 }
