edb77e966ff294eec42e0a207aa2bcd8fbaf7712
sedrickotolo <sedricksedu2@gmail.com>
2022-01-27 16:01:06 +0300

    android sqlite mysql sync complete

--- app/src/main/java/com/pkasemer/zodongofoods/LoginMaterial.java
+++ app/src/main/java/com/pkasemer/zodongofoods/LoginMaterial.java
@@ -52,7 +52,7 @@ public class LoginMaterial extends AppCompatActivity {
         //if the user is already logged in we will directly start the profile activity
         if (SharedPrefManager.getInstance(this).isLoggedIn()) {
             finish();
-            startActivity(new Intent(this, RootActivity.class));
+            startActivity(new Intent(this, Demo.class));
             return;
         }
 
---
+++ app/src/main/java/com/pkasemer/zodongofoods/Models/Name.java
@@ -0,0 +1,19 @@
+package com.pkasemer.zodongofoods.Models;
+
+public class Name {
+    private String name;
+    private int status;
+
+    public Name(String name, int status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
